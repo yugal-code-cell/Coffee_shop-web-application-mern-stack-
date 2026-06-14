3dab33f2b01c340435141916c15bd570a84c1249
yuvraj <yugal9180@gmail.com>
2026-06-14 08:18:40 +0000

    in process of seting of database

--- backend/server.js
+++ backend/server.js
@@ -16,40 +16,40 @@ app.use(express.json());
 // DATABASE
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log(
-      "MongoDB Connected"
-    );
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log(
+            "MongoDB Connected"
+        );
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 
 // CREATE DEFAULT TABLES
 
 const createTables = async () => {
-  const count =
-    await Table.countDocuments();
+    const count =
+        await Table.countDocuments();
 
-  if (count === 0) {
-    const tables = [];
+    if (count === 0) {
+        const tables = [];
 
-    for (let i = 1; i <= 10; i++) {
-      tables.push({
-        tableNumber: i,
-      });
+        for (let i = 1; i <= 10; i++) {
+            tables.push({
+                tableNumber: i,
+            });
+        }
+
+        await Table.insertMany(
+            tables
+        );
+
+        console.log(
+            "10 Tables Created"
+        );
     }
-
-    await Table.insertMany(
-      tables
-    );
-
-    console.log(
-      "10 Tables Created"
-    );
-  }
 };
 
 createTables();
@@ -58,27 +58,24 @@ createTables();
 // ROUTES
 
 app.use(
-  "/api/tables",
-  require("./routes/tableRoutes")
+    "/api/tables",
+    require("./routes/tableRoutes")
 );
 
 
 // TEST ROUTE
 
 app.get("/", (req, res) => {
-  res.send(
-    "Coffee Shop Backend Running"
-  );
+    res.send(
+        "Coffee Shop Backend Running"
+    );
 });
 
 
 // SERVER
 
-const PORT =
-  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server Running On Port ${PORT}`
-  );
+    console.log(`Server Running On Port ${PORT}`);
 });
